80c987b58420a13565f3f1704bc7adbb177b5b86
songqian6 <songqian6@jd.com>
2019-12-30 18:29:25 +0800

    debug tail

--- tail.go
+++ tail.go
@@ -220,7 +220,7 @@ func (tail *Tail) newReadLine(delim byte) (string, error) {
 		return line, err
 	}
 
-	line = strings.TrimRight(line, delim)
+	line = strings.TrimRight(line, string(delim))
 
 	return line, err
 }
@@ -236,7 +236,7 @@ func (tail *Tail) readLine() (string, error) {
 		return line, err
 	}
 
-	line = strings.TrimRight(line, '\n')
+	line = strings.TrimRight(line, "\n")
 
 	return line, err
 }
